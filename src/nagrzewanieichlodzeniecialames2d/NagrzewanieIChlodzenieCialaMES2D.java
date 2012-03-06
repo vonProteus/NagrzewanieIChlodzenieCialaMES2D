3b7a2f1480a0fbaeba50d3b1903ded9143a2707e
Maciej Krok <mkrok@metal.agh.edu.pl>
2012-03-06 08:55:32 +0100

    init

---
+++ src/nagrzewanieichlodzeniecialames2d/NagrzewanieIChlodzenieCialaMES2D.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nagrzewanieichlodzeniecialames2d;
+
+/**
+ *
+ * @author proteus
+ */
+public class NagrzewanieIChlodzenieCialaMES2D {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	// TODO code application logic here
+    }
+}
