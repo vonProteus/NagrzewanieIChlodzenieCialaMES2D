6547671e2eb62d5a4950c41219ac8446a27ca63e
Maciej Krok <mkrok@metal.agh.edu.pl>
2012-03-11 15:47:54 +0100

    element

---
+++ src/nagrzewanieichlodzeniecialames2d/data/Element.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nagrzewanieichlodzeniecialames2d.data;
+
+/**
+ *
+ * @author proteus
+ */
+public class Element {
+    
+    private int[] nop = new int[4];
+    
+    private int Npov;
+    
+    private int[] aPov = new int[2];
+
+    //<editor-fold defaultstate="collapsed" desc="seters&geters">
+    
+    public int getNpov() {
+	return Npov;
+    }
+
+    public void setNpov(int Npov) {
+	this.Npov = Npov;
+    }
+
+    public int[] getaPov() {
+	return aPov;
+    }
+
+    public void setaPov(int[] aPov) {
+	this.aPov = aPov;
+    }
+
+    public int[] getNop() {
+	return nop;
+    }
+
+    public void setNop(int[] nop) {
+	this.nop = nop;
+    }
+    //</editor-fold>
+    
+    
+}
