1a228e6c7d7209fcf8ba59d404d546abfa3e8910
Maciej Krok <mkrok@metal.agh.edu.pl>
2012-03-11 16:17:55 +0100

    Cor_L

---
+++ src/nagrzewanieichlodzeniecialames2d/data/Cor_L.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nagrzewanieichlodzeniecialames2d.data;
+
+/**
+ *
+ * @author proteus
+ */
+public class Cor_L {
+
+    private double N;
+    private double E;
+
+    //<editor-fold defaultstate="collapsed" desc="geters&seters">
+    public double getE() {
+	return E;
+    }
+
+    public void setE(double E) {
+	this.E = E;
+    }
+
+    public double getN() {
+	return N;
+    }
+
+    public void setN(double N) {
+	this.N = N;
+    }
+    //</editor-fold>
+}
