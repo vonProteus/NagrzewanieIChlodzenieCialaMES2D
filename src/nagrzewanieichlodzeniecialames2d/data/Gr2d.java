b42b270a382345ea484134bbb438948af5f7f1d8
Maciej Krok <mkrok@metal.agh.edu.pl>
2012-03-11 16:12:24 +0100

    Gr2d

---
+++ src/nagrzewanieichlodzeniecialames2d/data/Gr2d.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nagrzewanieichlodzeniecialames2d.data;
+
+/**
+ *
+ * @author proteus
+ */
+public class Gr2d {
+
+    private int nh;
+    private int ne;
+    private int nbn;
+    private int ncn;
+    private int nhPov;
+    private Element[] EL;
+    private Node[] ND;
+
+    //<editor-fold defaultstate="collapsed" desc="geters&setters">
+    public Element[] getEL() {
+	return EL;
+    }
+
+    public void setEL(Element[] EL) {
+	this.EL = EL;
+    }
+
+    public Node[] getND() {
+	return ND;
+    }
+
+    public void setND(Node[] ND) {
+	this.ND = ND;
+    }
+
+    public int getNbn() {
+	return nbn;
+    }
+
+    public void setNbn(int nbn) {
+	this.nbn = nbn;
+    }
+
+    public int getNcn() {
+	return ncn;
+    }
+
+    public void setNcn(int ncn) {
+	this.ncn = ncn;
+    }
+
+    public int getNe() {
+	return ne;
+    }
+
+    public void setNe(int ne) {
+	this.ne = ne;
+    }
+
+    public int getNh() {
+	return nh;
+    }
+
+    public void setNh(int nh) {
+	this.nh = nh;
+    }
+
+    public int getNhPov() {
+	return nhPov;
+    }
+
+    public void setNhPov(int nhPov) {
+	this.nhPov = nhPov;
+    }
+    //</editor-fold>
+}
